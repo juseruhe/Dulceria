dc03759836a649793ae0749bdaf4ebf1cb9e3777
Sebastian <jsruiz7411@misena.edu.co>
2020-08-22 13:59:00 -0500

    script: Aproximaciones POO

---
+++ Aproximaciones/Aproximación 1/Producto.java
@@ -0,0 +1,10 @@
+public class Producto {
+    String  Codigo_Producto;
+    String Nombre_Producto;
+    int Precio_Producto;
+    int Cantidad_Producto;
+    int Unidades_Vendidas;
+    int Total_Ventas;
+
+
+}
---
+++ Aproximaciones/Aproximación 1/Ventas.java
@@ -0,0 +1,62 @@
+
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Ventas {
+    
+    public static void main(String[] args) {
+        
+    Scanner Entrada = new Scanner(System.in);
+
+     int Productos_Registrados;
+
+     Producto[]  losProductos =  new Producto[50];
+
+     String Codigo,Nombre;
+
+     int Precio,Cantidad,Unidades_Vendidas,Total_Producto,Total_Productos=0;
+
+     
+
+     
+    Productos_Registrados = Integer.parseInt(JOptionPane.showInputDialog("Digite la Cantidad de Productos que desea registrar :")) ;
+
+    for (int i= 1 ; i<=Productos_Registrados; i++) {
+
+    Codigo = JOptionPane.showInputDialog("Digite el Código del Producto");
+    Nombre = JOptionPane.showInputDialog("Digite el Nombre del Producto");
+    Precio = Integer.parseInt(JOptionPane.showInputDialog("Digite el Precio del Producto"));
+    Cantidad = Integer.parseInt(JOptionPane.showInputDialog("Digite la cantidad del Producto"));
+    Unidades_Vendidas = Integer.parseInt(JOptionPane.showInputDialog("Digite las unidades vendidas del Producto"));
+    Total_Producto = Cantidad * Unidades_Vendidas;
+
+      JOptionPane.showMessageDialog(null,"El Total de ventas del " + Nombre+ " es de : " + Total_Producto);
+
+    
+    Total_Productos+=Total_Producto;
+
+    Producto unproducto = new Producto();
+    unproducto.Codigo_Producto = Codigo;
+    unproducto.Nombre_Producto = Nombre;
+    unproducto.Precio_Producto = Precio;
+    unproducto.Cantidad_Producto = Cantidad;
+    unproducto.Unidades_Vendidas = Unidades_Vendidas;
+    unproducto.Total_Ventas = Total_Producto;
+
+    losProductos[i] = unproducto;
+
+   
+
+
+
+    }
+
+
+    JOptionPane.showMessageDialog(null,"El Total de Ventas de Todos los Productos es:  " + Total_Productos);
+
+
+}
+
+}
