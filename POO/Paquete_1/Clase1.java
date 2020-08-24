7e4438941f53fb3639bf845c3d5596310645471a
Sebastian <jsruiz7411@misena.edu.co>
2020-08-24 15:24:05 -0500

    script:Getters and Setters

--- POO/Paquete_1/Clase1.java
+++ POO/Paquete_1/Clase1.java
@@ -1,5 +1,37 @@
 public class Clase1 {
     
 // Modificadores de Acceso
+/*public int Atributo1;
+
+private int Atributo2;*/
+
+private int edad;
+
+private String nombre;
+
+public void Setedad(int edad){
+this.edad = edad;
+
+}
+
+public int Getedad(){
+
+    return edad;
+}
+
+
+public void Setnombre(String nombre){
+
+this.nombre = nombre;
+
+
+}
+
+
+public String Getnombre(){
+
+    return nombre;
+    
+}
 
 }
--- POO/Paquete_1/Clase2.java
+++ POO/Paquete_1/Clase2.java
@@ -1,8 +1,21 @@
 public class Clase2 {
     
 public static void main(String[] args) {
-    
-    
+    /*
+    Clase1 Objeto1 = new Clase1();
+
+   Objeto1.Atributo1 = 15;*/
+
+   Clase1 Objeto1 = new Clase1();
+
+   Objeto1.Setedad(10);
+   Objeto1.Setnombre("Juan");
+
+   System.out.println("La Edad del Usuario es: "+Objeto1.Getedad());
+
+   System.out.println("El nombre del Usuario es: "+Objeto1.Getnombre());
+
+
 }
 
 
