1a7740100c439ffcb01addb2a4f423590206e96a
Sebastian <jsruiz7411@misena.edu.co>
2020-08-31 16:23:59 -0500

    Base de Datos Creada

---
+++ BaseDatosConJava/BD/Dulceria.sql
@@ -0,0 +1,12 @@
+create database Dulceria;
+use Dulceria;
+
+create table Producto(
+    id int(15) not null AUTO_INCREMENT,
+    codigo_producto varchar(15) not null,
+    nombre_producto varchar(55) not null,
+    categoria varchar(55) not null,
+    venta_dia1 int(30) not null,
+    venta_dia2 int(30) not null,
+    venta_dia3 int(30) not null
+    );
--- Ejercicio Base/Código Java/Total_Ventas/Total_Ventas.java
+++ Ejercicio Base/Código Java/Total_Ventas/Total_Ventas.java
@@ -1,4 +1,4 @@
-package Total_Ventas;
+
 
 import java.util.Scanner;
 
