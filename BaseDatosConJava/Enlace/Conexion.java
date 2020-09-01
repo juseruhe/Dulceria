2ebd0a472b7a78e04fe63c23ea24016bbae077d0
Sebastian <jsruiz7411@misena.edu.co>
2020-08-31 19:08:21 -0500

    script: ViewProducto.java

---
+++ BaseDatosConJava/Enlace/Conexion.java
@@ -0,0 +1,37 @@
+package BaseDatosConJava.Enlace;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion{
+
+    public static Connection conectar(){
+
+      Connection con = null;
+
+     String usuario = "root";
+     String password = "";
+     String url = "jdbc:mysql://localhost:3306/dulceria?user="+usuario+"&password="+password;
+
+     try{
+          con = DriverManager.getConnection(url);
+
+          if(con != null){
+         
+            System.out.println("Conectado a la Base de Datos");
+          }
+}catch(SQLException e){
+
+    System.out.println("No se pudo conectar a la Base de Datos");
+
+
+    e.printStackTrace();
+     }
+
+
+return con;
+
+    }
+
+
+}
---
+++ BaseDatosConJava/Enlace/IProductoDAO.java
@@ -0,0 +1,11 @@
+package BaseDatosConJava.Enlace;
+import java.util.List;
+
+public interface IProductoDAO {
+    public boolean registrar(Producto producto);
+    public List<Producto>obtener();
+    public boolean actualizar(Producto producto);
+    public boolean eliminar(Producto producto);
+
+
+}
